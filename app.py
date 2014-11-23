03604b9b3c2cfeb25aced19182da0fe93b7af500
Dmitry Olshansky <dmitry.olsh@gmail.com>
2014-11-23 22:59:54 +0300

    Allow reading from stdin in FCSP binary, add tiny Web-app in Flask

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=UTF-8
+from flask import Flask, request, Response, render_template
+import subprocess
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return app.send_static_file("index.html")
+
+@app.route("/encode", methods=["POST"])
+def projects_list():
+    f = request.files['file']
+    data = f.read()
+    print data
+    proc = subprocess.Popen("./fcsp -d descriptors".split(),
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    data = proc.communicate(data)    
+    return data
+
+if __name__ == '__main__':
+    import sys
+    reload(sys)    
+    sys.setdefaultencoding("utf-8")
+    app.run(debug=True)
